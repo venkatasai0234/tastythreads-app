6ac8177df2e9d3b2df4a0bb70013b6d4688c3965
venkatasai0234 <venkatasaiyedupati080@gmail.com>
2025-05-03 16:19:55 -0700

    routes_update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>🍔 TastyThreads Frontend</h1>} />
+      <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
